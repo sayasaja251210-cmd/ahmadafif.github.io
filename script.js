52ff9b28f66860504f04fee056551b41165c1c35
sayasaja251210-cmd <sayasaja251210@gmail.com>
2026-09-10 09:56:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Student at SMK Krian 1', 'Software Engineering Student'],
+    strings: ['Siswa SMK Krian 1', 'Siswa Rekayasa Perangkat Lunak'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
